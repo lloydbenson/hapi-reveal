81b190d72b9e44c420c87d9d4764f26a2aded241
lloydbenson <lloyd.benson@gmail.com>
2015-04-24 12:31:38 -0500

    minor fixes

--- helpers/loadPartial.js
+++ helpers/loadPartial.js
@@ -3,8 +3,6 @@ var Handlebars = require('handlebars');
 
 module.exports = function (filename) {
 
-    console.log(filename);
-    console.log(typeof filename);
     if (!Handlebars.partials[filename]) {
         Handlebars.registerPartial(filename, Fs.readFileSync(filename));
     }
